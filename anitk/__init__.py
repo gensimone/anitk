f5729364907d56a5b890fbe7a31acdeca002a26d
Simone Gentili <gensimone.git@gmail.com>
2025-11-30 15:14:48 +0100

    Add __init__.py

---
+++ anitk/__init__.py
@@ -0,0 +1,19 @@
+from .base import BaseFrame, Request, Direction
+from .slider import SlideFrame
+from .resizable import ResizableFrame
+from .enums import SlideDirection, Axis, Orientation, Direction
+
+
+__all__ = [
+    "BaseFrame", 
+    "Request", 
+
+    "SlideFrame",
+
+    "ResizableFrame",
+
+    "SlideDirection",
+    "Axis",
+    "Orientation",
+    "Direction"
+]
